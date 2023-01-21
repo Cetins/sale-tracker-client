94e8a9bb40e756b808a335a6079992dc71e45392
Cetins <69481095+Cetins@users.noreply.github.com>
2023-01-21 15:48:50 +0000

    findOne added for product

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,38 +1,43 @@
 const baseURL = 'http://localhost:9000/api/products/'
 
 const ProductService = {
-    getProducts() {
-      return fetch(baseURL)
-      .then(res => res.json());
-    },
+  getProduct(id) {
+    return fetch(baseURL + id)
+    .then(res => res.json());
+  },
 
-    updateProduct(product) {
-      const updateURL = baseURL + product._id;
-      delete product._id
-      return fetch(updateURL, {
-        method: 'PUT',
-        body: JSON.stringify(product),
-        headers: {'Content-Type': 'application/json'}
-      })
-      .then(res => res.json());
-    }, 
+  getProducts() {
+    return fetch(baseURL)
+    .then(res => res.json());
+  },
 
-    addProduct(product) {
-      return fetch(baseURL, {
-        method: 'POST',
-        body: JSON.stringify(product),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(res => res.json());
-    },
+  updateProduct(product) {
+    const updateURL = baseURL + product._id;
+    delete product._id
+    return fetch(updateURL, {
+      method: 'PUT',
+      body: JSON.stringify(product),
+      headers: {'Content-Type': 'application/json'}
+    })
+    .then(res => res.json());
+  }, 
 
-    deleteProduct(id) {
-      return fetch(baseURL + id, {
-        method: 'DELETE'
-      });
-    }
+  addProduct(product) {
+    return fetch(baseURL, {
+      method: 'POST',
+      body: JSON.stringify(product),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => res.json());
+  },
+
+  deleteProduct(id) {
+    return fetch(baseURL + id, {
+      method: 'DELETE'
+    });
+  }
 }
 
 export default ProductService;
