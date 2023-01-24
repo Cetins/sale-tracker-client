bd617b42bcfd9baa5be8d64d265f3eb15a3b940c
Cetins <69481095+Cetins@users.noreply.github.com>
2023-01-24 11:15:26 +0000

    new staff component added

--- src/components/dashboard/Staff.js
+++ src/components/dashboard/Staff.js
@@ -2,6 +2,8 @@ import React from 'react';
 import StaffService from '../../services/StaffService';
 import SubTitle from '../SubTitle';
 import StaffTable from './StaffTable';
+import { FaPlus } from 'react-icons/fa';
+import { Link } from "react-router-dom";
 
 
 const Staff = ({ staff }) => {
@@ -9,6 +11,7 @@ const Staff = ({ staff }) => {
     <div className='child-container'>
         <SubTitle title="Staff Members"/>
         <StaffTable staff={staff} />
+        <button><Link to="/staff-new"><FaPlus className="icon"/>New Staff Member</Link></button>
     </div>
   )
 }
