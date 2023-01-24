9dcf7c79333364ed70ffddb5a8cb5bc3eb7e9152
Cetins <69481095+Cetins@users.noreply.github.com>
2023-01-24 20:59:47 +0000

    now sales also passed to calculate total for staff

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -5,11 +5,13 @@ import Staff from "../components/dashboard/Staff";
 import ProductService from "../services/ProductService";
 import ServiceService from "../services/ServiceService";
 import StaffService from '../services/StaffService';
+import SalesService from '../services/SalesService';
 
 const Dashboard = () => {
     const [products, setProducts] = useState([]);
     const [services, setServices] = useState([]);
     const [staff, setStaff] = useState([]);
+    const [sales, setSales] = useState([]);
 
     useEffect(() => {
         ProductService.getProducts()
@@ -23,12 +25,16 @@ const Dashboard = () => {
         StaffService.getStaff()
             .then(res => setStaff(res));
     }, []);
+    useEffect(() => {
+        SalesService.getSales()
+            .then(res => setSales(res));
+    }, []);
 
     return (
         <div className="parent-container">
             <Products products={products}/>
             <Services services={services}/>
-            <Staff staff={staff}/>
+            <Staff staff={staff} sales={sales}/>
         </div>
     )
 }
