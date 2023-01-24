24cb6866887cc5f3adedb43ecd49c9f1db6b2e6f
Cetins <69481095+Cetins@users.noreply.github.com>
2023-01-24 10:59:21 +0000

    findone method added, id deleted before update obj

--- src/services/StaffService.js
+++ src/services/StaffService.js
@@ -1,38 +1,43 @@
 const baseURL = 'http://localhost:9000/api/staff/'
 
 const StaffService = {
-    getStaff() {
-        return fetch(baseURL)
-            .then(res => res.json());
-    },
+  getStaffMember(id) {
+    return fetch(baseURL + id)
+    .then(res => res.json());
+  },
 
-    updateStaff(member) {
-        return fetch(baseURL + member._id, {
-            method: 'PUT',
-            body: JSON.stringify(member),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-            .then(res => res.json());
-    },
+  getStaff() {
+    return fetch(baseURL)
+        .then(res => res.json());
+  },
 
-    addStaff(member) {
-        return fetch(baseURL, {
-          method: 'POST',
-          body: JSON.stringify(member),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        })
-          .then(res => res.json());
-    },
+  updateStaff(member) {
+    const updateURL = baseURL + member._id;
+    delete member._id
+    return fetch(updateURL, {
+      method: 'PUT',
+      body: JSON.stringify(member),
+      headers: {'Content-Type': 'application/json'}
+    })
+    .then(res => res.json());
+  },
 
-    deleteStaff(id) {
-        return fetch(baseURL + id, {
-          method: 'DELETE'
-        });
-    }
+  addStaff(member) {
+    return fetch(baseURL, {
+      method: 'POST',
+      body: JSON.stringify(member),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json());
+  },
+
+  deleteStaff(id) {
+    return fetch(baseURL + id, {
+      method: 'DELETE'
+    });
+  }
 }
 
 export default StaffService;
