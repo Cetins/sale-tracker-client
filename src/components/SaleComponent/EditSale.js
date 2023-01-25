ea2954254a09aafb394ecad55ffdb68a1ccca082
Cetins <69481095+Cetins@users.noreply.github.com>
2023-01-25 11:19:29 +0000

    edit sale with prompt

--- src/components/SaleComponent/EditSale.js
+++ src/components/SaleComponent/EditSale.js
@@ -4,6 +4,7 @@ import SaleService from '../../services/SalesService';
 import StaffService from '../../services/StaffService';
 import ServiceService from '../../services/ServiceService';
 import ProductServices from '../../services/ProductService';
+import ShopService from '../../services/ShopService';
 import { MdDeleteForever } from 'react-icons/md';
 
 const EditSale = () => {
@@ -12,7 +13,8 @@ const EditSale = () => {
     const [services, setServices] = useState(null);
     const [products, setProducts] = useState(null);
     const [staff, setStaff] = useState(null);
-    const navigate = useNavigate();
+    const [shop, setShop] = useState(null);
+    const navigate = useNavigate(null);
     let serviceOptions;
     let productOptions;
     let staffOptions;
@@ -36,17 +38,26 @@ const EditSale = () => {
         StaffService.getStaff()
             .then(res => setStaff(res))
     }, []);
+
+    useEffect(() => {
+        ShopService.getShop()
+            .then(res => setShop(res))
+    }, []);
     
     const handleServiceChange = () => {};
     const handleProductChange = () => {};
     const handleStaffChange = () => {};
     const handleDateChange = () => {};
-
+    
     const handleUpdateSaleSubmit = (e) => {
         e.preventDefault();
-        SaleService.updateSales(sale)
-        .then(alert("Sale Updated"))
-        .then(navigate("/dashboard"));
+        const passCode = prompt("Please enter your code to confirm");
+        if(passCode === shop.passCode) {
+            SaleService.updateSales(sale)
+            .then(navigate("/dashboard"));
+        } else {
+            alert("Sorry your passCode is not correct/valid")
+        }
     }
     
     const handleDeleteSale = (e) => {
@@ -60,6 +71,7 @@ const EditSale = () => {
     if (services === null) { return (<div>Loading Services...</div>) }
     if (products === null) { return (<div>Loading Products...</div>) }
     if (staff === null) { return (<div>Loading Staff...</div>) }
+    if (shop === null) { return (<div>Loading Shop...</div>) }
 
     staffOptions = staff.map(item => <option key={item._id}>{item.name}</option>)
     serviceOptions = services.map(item => <option key={item._id}>{item.title}</option>)
@@ -96,6 +108,7 @@ const EditSale = () => {
                     <label>Date:</label>
                     <input type="date" defaultValue={sale.date} onChange={handleDateChange} />
                 </div>
+                <input type="submit" value="Update Sale"/>
             </form>
             <button onClick={handleDeleteSale}><MdDeleteForever className='icon'/>Delete Sale</button>
         </div>
